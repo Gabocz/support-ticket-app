56accee3084e74b7401d986875c044bc191e3ca6
Gabocz <gabriella.ando@gmail.com>
2023-02-26 18:01:16 +0100

    commit before deploy

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,37 @@
-console.log('Server...')
+const path = require('path')
+const express = require('express')  // common JS module syntax as opposed to the React syntax (ES2015 syntax with import keyword)
+const colors = require('colors')
+const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+const PORT = process.env.PORT || 8000
+
+// Connect to database
+connectDB()
+
+const app = express()
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: false })) // for parsing application/x-www-form-urlencoded
+
+
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'))
+app.use('/api/tickets', require('./routes/ticketRoutes'))
+
+// Serve Frontend
+if(process.env.NODE_ENV === 'production') {
+    // Set build folder as static
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+    app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'))
+} else {
+    app.get('/', (req, res) => {
+        res.status(200).json({ message: 'Welcome to the support desk API'})
+    })
+}
+
+app.use(errorHandler)
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
